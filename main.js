04e44f97ce9d4a92f1a8c8ed9797af6ad19d6eb4
Tony Eder <tonyeder11@gmail.com>
2023-10-26 22:44:19 -0700

    working through. Now for elements of successful message

--- main.js
+++ main.js
@@ -3,6 +3,60 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// Add .hidden class to error message
+let error = document.querySelector('#modal');
+error.className = "hidden";
+
+// Add eventListener to empty likeGlyph
+const likeGlyph = document.querySelectorAll('.like-glyph');
+likeGlyph.forEach(function (likeGlyph) {
+  likeGlyph.addEventListener('click', function () {
+    mimicServerCall()
+      .then(res => {
+        console.log(res);
+      })
+      //When the "server" returns a failure status:
+      // Respond to the error using a .catch(() => {}) block after your .then(() => {}) block.
+      .catch(issue => {
+        // Display the server error message in the modal
+        error.textContent = issue.toUpperCase();
+        console.log(issue)
+        // Display the error modal by removing the .hidden class
+        error.classList.remove('hidden')
+        setTimeout(function(){
+          // Use setTimeout to hide the modal after 3 seconds (add the .hidden class)
+          error.className = "hidden"
+        }, 3000)
+        // Perform frontend actions based on the error
+      });
+  });
+});
+
+
+
+
+
+
+
+// const likeGlyphs = document.querySelectorAll('.like-glyph');
+
+// // Add a click event listener to each "like-glyph" element
+// likeGlyphs.forEach(function (likeGlyph) {
+//   likeGlyph.addEventListener('click', function () {
+//     // Invoke mimicServerCall when a "like-glyph" element is clicked
+//     mimicServerCall()
+//       .then(function (response) {
+//         // Handle a successful response here
+//         console.log('Server response:', response);
+//         // Perform frontend actions based on the response
+//       })
+//       .catch(function (error) {
+//         // Handle an error if mimicServerCall fails
+//         console.error('Error:', error);
+//         // Perform frontend actions based on the error
+//       });
+//   });
+// });
 
 
 
@@ -11,9 +65,9 @@ const FULL_HEART = '♥'
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
